7a04d878e21d31ef8074f42642404c9043bb1f7f
kukhvan <satoshy@ya.ru>
2016-10-04 09:39:55 +0300

    added right sidenav

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,3 +1,9 @@
 'use strict'
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngMaterial', 'ngAnimate']);
+
+app.controller('AppCtrl', function ($scope, $mdSidenav) {
+  $scope.openRightMenu = function() {
+    $mdSidenav('right').toggle();
+  };
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require angular
+//= require angular-material
+//= require angular-messages
+//= require angular-animate
+//= require angular-aria
 //= require turbolinks
 //= require_tree .
